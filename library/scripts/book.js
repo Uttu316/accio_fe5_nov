9c9de63c98da355947d4888b49a8cd4ec352957b
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-11-15 22:07:46 +0530

    Library Project

--- library/scripts/book.js
+++ library/scripts/book.js
@@ -3,7 +3,7 @@ const getBook = async () => {
 
   try {
     if (!bookId) throw new Error("BookId is not valid"); // if no book id in URL then throw
-
+    showLoader();
     const res = await api({
       path: "/books/" + bookId,
     }); // load data for a book with given id
@@ -11,14 +11,17 @@ const getBook = async () => {
     if (!res || (res && !res.data)) throw new Error("BookId is not valid"); // if no data found then throw
 
     let book = res.data; // data found
+    hideLoader();
+    showDetails();
     showBook(book); // show book data
   } catch (e) {
     notFound(e);
   }
 };
 const notFound = (e) => {
-  const message = e.message;
-  $("#book_detials").html(`${message},Go to Home`);
+  console.log(e.message);
+  hideLoader();
+  showError();
 };
 
 const showBook = (book) => {
@@ -30,11 +33,83 @@ const showBook = (book) => {
     authors,
     imageLinks,
     description,
-    ratingsCount = 5,
+    publisher,
     publishedDate,
+    categories,
+    previewLink,
+    infoLink,
   } = volumeInfo;
+
+  const { retailPrice } = saleInfo;
+  const { amount, countryCode = "INR" } = retailPrice || {};
+
   document.title = title;
   $("#book_title").html(title);
-  subtitle && $("#book_subtitle").html(subtitle);
+  if (subtitle) {
+    $("#book_subtitle").html(subtitle);
+  } else {
+    $("#book_subtitle").hide();
+  }
+
+  $("#book_description").html(description);
+
+  const authorsEl = $("#book_autor");
+  if (authors && authors.length) {
+    let authorsName = authors.join(", ");
+    authorsEl.html(authorsName);
+  }
+
+  const categoryEl = $("#book_categories");
+
+  if (categories && categories.length) {
+    let pills = categories.map((cname) => {
+      return $(`<p class="book_category"> ${cname}</p>`);
+    });
+    categoryEl.append(pills);
+  }
+
+  const publishEl = $("#book_publisher");
+
+  publishEl.html(publisher);
+
+  const publishDateEl = $("#book_published_date");
+
+  const date = new Date(publishedDate);
+  const formattedDate = date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+  publishDateEl.html(formattedDate);
+
+  const price = $("#book_price");
+  if (amount) {
+    price.removeClass("not_for_sale");
+    price.html(`${countryCode} ${Math.ceil(amount)}`);
+  }
+
+  const previewBtn = $("#book_preview");
+  let link = previewLink || infoLink;
+  if (link) {
+    previewBtn.on("click", () => {
+      window.open(link, "_blank").focus(); // open link in new tab
+    });
+  } else {
+    alert("Oops the preview not available");
+  }
+};
+
+const showLoader = () => {
+  $("#book_loader").show();
+};
+const hideLoader = () => {
+  $("#book_loader").hide();
+};
+
+const showError = () => {
+  $("#book_error").show();
+};
+const showDetails = () => {
+  $("#book_detials").show();
 };
 getBook();
