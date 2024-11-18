4fa1093b1be8cd20e5d3011dc343a8d979764b4a
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-11-18 23:10:52 +0530

    React app

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css"
+
+const App = ()=>{
+
+  return (
+    <div className="page">
+      <h1>Welcome to React</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ myapp/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import MyApp from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<MyApp />);
+
+
+
+
+
